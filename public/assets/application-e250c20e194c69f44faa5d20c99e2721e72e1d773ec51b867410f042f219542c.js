bb27b3454275ce6f32bd008c4064c25abb2cffea
Eric Holman <eric.l.holman.1107@icloud.com>
2017-11-20 17:31:51 -0700

    fixing error

--- public/assets/application-e250c20e194c69f44faa5d20c99e2721e72e1d773ec51b867410f042f219542c.js
+++ public/assets/application-e250c20e194c69f44faa5d20c99e2721e72e1d773ec51b867410f042f219542c.js
@@ -34586,7 +34586,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
   };
 
   return Dropdown;
-}($, Popper);
+};
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
